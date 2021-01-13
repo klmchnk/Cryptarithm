46876aca1086f83987f8a474b04220dd62c3cb80
AruchomuK <ashuskiya2@gmail.com>
2021-01-14 02:44:46 +0300

    Cryptarithm lab with O2 optimization

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #define MAX_LENGTH 100
-#define MAX_LETTERS 10
 #include <stdio.h>
 #include <string.h>
 #include <Windows.h>
@@ -12,16 +11,20 @@ struct Letter
 	char symbol;
 	int digit;
 	bool isFirst;
-} Letters[MAX_LETTERS];
+} Letters[10];
 
-int numberOfLetters = 0;
+int numberOfLetters = -1;
 long int variants = 0;
 
 int find_symbol(char s)
 {
-	for (int i = 0; i < MAX_LETTERS; i++)
+	for (int i = 0; i < 10; i += 2)
+	{
 		if (Letters[i].symbol == s)
 			return i;
+		if (Letters[i + 1].symbol == s)
+			return i + 1;
+	}
 	return -1;
 }
 
@@ -32,22 +35,25 @@ void swap(int i, int j)
 	Letters[j].digit = tmp;
 }
 
-void get_array_of_letters(char* str)
+void get_array_of_letters(char* str, int length)
 {
 	int i, idx;
-	for (i = 0; i < strlen(str); i++)
+	char c;
+
+	for (i = 0; i < length; i++)
 	{
-		if (str[i] > 64 && str[i] < 91)
+		c = str[i];
+		if (c > 64 && c < 91)
 		{
-			idx = find_symbol(str[i]);
+			idx = find_symbol(c);
 
 			if (idx == -1)
 			{
+				numberOfLetters++;
 				if (i == 0 || str[i - 1] == ' ')
 					Letters[numberOfLetters].isFirst = true;
-				Letters[numberOfLetters].symbol = str[i];
+				Letters[numberOfLetters].symbol = c;
 				Letters[numberOfLetters].digit = numberOfLetters;
-				numberOfLetters++;
 			}
 			else
 			{
@@ -57,19 +63,21 @@ void get_array_of_letters(char* str)
 		}
 	}
 
-	for (i = 9; i > numberOfLetters - 1; i--)
+	for (i = 9; i > numberOfLetters; i--)
 		Letters[i].digit = i;
 
 	swap(0, 1);
 }
 
-int check_sum(char* str)
+int check_sum(char* str, int length)
 {
 	int i = 0 , sum = 0, tmp = 0, answer = 0, idx = 0;
-	
-	for (i = strlen(str) - 1; str[i] != ' '; i--)
+	int pow = 1;
+	char c;
+
+	for (i = length - 1; (c = str[i]) != ' '; i--)
 	{
-		idx = find_symbol(str[i]);
+		idx = find_symbol(c);
 
 		if (Letters[idx].digit == 0)
 			if (Letters[idx].isFirst)
@@ -78,14 +86,15 @@ int check_sum(char* str)
 				return 0;
 			}
 
-		answer += Letters[idx].digit * pow(10, strlen(str) - 1 - i);
+		answer += Letters[idx].digit * pow;
+		pow *= 10;
 	}
 
-	for (i = 0; str[i] != '='; i++)
+	for (i = 0; (c = str[i]) != '='; i++)
 	{
-		if (str[i] > 64 && str[i] < 91)
+		if (c > 64 && c < 91)
 		{
-			idx = find_symbol(str[i]);
+			idx = find_symbol(c);
 
 			if (Letters[idx].digit == 0)
 				if (Letters[idx].isFirst)
@@ -112,20 +121,20 @@ int check_sum(char* str)
 	return 0;
 }
 
-void solution(char* str)
+void solution(char* str, int length)
 {
 	int lastLetterValue = 0;
 
 	while (1)
 	{
-		if(lastLetterValue != Letters[numberOfLetters - 1].digit)
-			if (check_sum(str))
+		if(lastLetterValue != Letters[numberOfLetters].digit)
+			if (check_sum(str, length))
 				return;
 
-		lastLetterValue = Letters[numberOfLetters - 1].digit;
+		lastLetterValue = Letters[numberOfLetters].digit;
 
-		int j = MAX_LETTERS - 2;
-		int k = MAX_LETTERS - 1;
+		int j = 8;
+		int k = 9;
 
 		while (j != -1 && Letters[j].digit >= Letters[j + 1].digit)
 			j--;
@@ -136,21 +145,24 @@ void solution(char* str)
 		swap(j, k);
 
 		int l = j + 1;
-		int r = MAX_LETTERS - 1;
+		int r = 9;
 
 		while (l < r)
 			swap(l++, r--);
 	}
 }
 
-void print_answer(char* str)
+void print_answer(char* str, int length)
 {
-	for (int i = 0; i < strlen(str); i++)
+	char c;
+
+	for (int i = 0; i < length; i++)
 	{
-		if (str[i] > 64 && str[i] < 91)
-			printf("%d", Letters[find_symbol(str[i])].digit);
+		c = str[i];
+		if (c > 64 && c < 91)
+			printf("%d", Letters[find_symbol(c)].digit);
 		else
-			printf("%c", str[i]);
+			printf("%c", c);
 	}
 }
 
@@ -158,6 +170,8 @@ int main()
 {
 	char string[MAX_LENGTH];
 	gets(string);
+	
+	int length = strlen(string);
 
 	LARGE_INTEGER freq, t1, t2;
 	double el_t;
@@ -165,14 +179,14 @@ int main()
 	QueryPerformanceFrequency(&freq);
 	QueryPerformanceCounter(&t1);
 
-	get_array_of_letters(string);
-	solution(string);
+	get_array_of_letters(string, length);
+	solution(string, length);
 
 	QueryPerformanceCounter(&t2);
 	el_t = t2.QuadPart - t1.QuadPart;
 	el_t = el_t / freq.QuadPart;
 
-	print_answer(string);
+	print_answer(string, length);
 
 	printf("\n\nThe time to solve the rebus: %f seconds \n", el_t);
 	printf("\nThe solution variant number: %ld", variants);
